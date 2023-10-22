86b9fcbc26a28d3ed69b5d0921e111594d6750c5
Darya <watashi.no.kao24@gmail.com>
2023-10-22 22:41:32 +0300

    finish

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,36 @@
 class GuessingGame {
-    constructor() {}
+    min = 0;
+    max = 0;
+    num = 0
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
 
     guess() {
-
+        this.num = Math.ceil((this.max - this.min) / 2 + this.min)
+        return this.num
     }
 
     lower() {
+        this.max = this.num
 
     }
 
     greater() {
+        this.min = this.num
 
     }
 }
 
 module.exports = GuessingGame;
+
+
+
+
+
+
+
